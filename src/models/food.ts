259ffe293d970bd7978902f8712a10faa7c5d13f
Julien Ouvrard <julien.ouvrard311@gmail.com>
2018-06-07 00:46:35 +0200

    Add ingredient array for food

--- src/models/food.ts
+++ src/models/food.ts
@@ -21,6 +21,7 @@ export var FoodSchema: Schema = new Schema({
   price: { type: Number, default: 0, required: true },
   needCooking: { type: Boolean, default: false, required: true },
   hasIngredients: { type: Boolean, default: false, required: true },
+  ingredients: {type: [String]},
   currency: { type: String, default: "EUR", required: true },
   created: { type: Date },
   updated: { type: Date }
